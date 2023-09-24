f9a1a943c470100d2deb70c79a5e3dc5d33d1b66
Ch.Tejaswini <tejaswinich94@gmail.com>
2023-09-24 15:56:30 +0530

    Expense script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo Installing Nginx
 dnf install nginx -y &>>$log_file
 
 echo Placing Expence Confing File in Nginx
-cp expence.conf /etc/nginx/default.d/expence.conf &>>$log_file
+cp expence.conf /etc/nginx/default.d/expence.conf &&>>$log_file
 
 echo Removing Old Nginx content
 rm-rf /usr/share/nginx/html/* &>>$log_file
@@ -15,5 +15,5 @@ cd /usr/share/nginx/html
 download_and_extract
 
 echo Starting Nginx Service
-systemctl enable nginx &>>$log_file
-systemctl start nginx &>>$log_file
+systemctl enable nginx &&>>$log_file
+systemctl start nginx &&>>$log_file
--- mysql.sh
+++ mysql.sh
@@ -10,8 +10,8 @@ echo Instal MySQL Server
 dnf install mysql-community-server -y &>>$log_file
 
 echo Start MySQL Service
-systemctl enable mysqld &>>$log_file
-systemctl start mysqld &>>$log_file
+systemctl enable mysqld &&>>$log_file
+systemctl start mysqld &&>>$log_file
 
 echo Setup root Password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
