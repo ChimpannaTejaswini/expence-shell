512100edd06bf0e28e9b5750b2a2b950ad52e577
Ch.Tejaswini <tejaswinich94@gmail.com>
2023-09-24 13:25:43 +0530

    Expense script

--- common.sh
+++ common.sh
@@ -4,7 +4,6 @@ download_and_extract() {
   echo Download $component code
   curl -s -o /tmp/$component https://expence-artifacts.s3.amazonaws.com/$component.zip >>$log_file
 
-
   echo Extracting $component Code
   unzip /tmp/$component.zip >>$log_file
 }
--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,6 @@
 Source common.sh
 component=frontend
 
-log_file=/tmp/expence.log
 echo Installing Nginx
 dnf install nginx -y >>$log_file
 
@@ -10,6 +9,7 @@ cp expence.conf /etc/nginx/default.d/expence.conf >>$log_file
 
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* >>$log_file
+
 cd /usr/share/nginx/html
 
 download_and_extract
