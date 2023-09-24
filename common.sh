c82561335501ad7f5ef593ac5f74265e66d166ac
Ch.Tejaswini <tejaswinich94@gmail.com>
2023-09-24 16:29:25 +0530

    Expense script

--- common.sh
+++ common.sh
@@ -3,7 +3,10 @@ log_file=/tmp/expence.log
 download_and_extract() {
   echo Download $component code
   curl -s -o /tmp/$component.zip https://expence-artifacts.s3.amazonaws.com/$component.zip >>$log_file
+  echo $?
 
   echo Extracting $component Code
   unzip /tmp/$component.zip &>>$log_file
+  echo $?
+
 }
