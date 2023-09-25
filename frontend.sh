f69d564cbbcdb70537655ddd26ca77e064ff1e06
Ch.Tejaswini <tejaswinich94@gmail.com>
2023-09-25 22:01:13 +0530

    Expense script

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,18 @@
 source common.sh
 component=frontend
 
+
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
+stat_check()
 
 echo Placing Expence Config File in Nginx
 cp expence.conf /etc/nginx/default.d/expence.conf &&>>$log_file
+stat_check()
 
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
+stat_check()
 
 cd /usr/share/nginx/html
 
@@ -18,3 +22,4 @@ download_and_extract
 echo Starting Nginx Service
 systemctl enable nginx &&>>$log_file
 systemctl start nginx &&>>$log_file
+stat_check()
